2a03295fcde448507d142440103c2910ba3a3ce1
Pavel Iohin <pavel@iohin.ru>
2020-07-09 15:04:49 +0200

    init commit

---
+++ build/generated/source/buildConfig/release/co/adcel/adcelpangle/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package co.adcel.adcelpangle;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "co.adcel.adcelpangle";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "3.1.0.0";
+}
